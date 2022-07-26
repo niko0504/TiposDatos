d9f99c9e29d66a6f97d861dd1ae827ba1521114e
niko0504 <75997193+niko0504@users.noreply.github.com>
2022-07-27 00:18:22 +0200

    Update TiposDatos.java

--- src/com/company/TiposDatos.java
+++ src/com/company/TiposDatos.java
@@ -1,2 +1,34 @@
-package com.company;public class TiposDatos {
+package com.company;
+public class TiposDatos {
+  
+    public static void main(String[] args){
+            // tipo identificador = 30;
+
+            //enteros
+            byte number1 = 1; // 1 byte
+            short number2= 2; // 2byte
+            int number3= 3; // 4byte
+            long number4= 4; // 8byte
+
+            //punto flotante
+            float decimal1 = 4.9f;
+            double decimal2 =9.99d;
+
+            //caracter
+            char caracter1 = 'a'; //Solo una letra
+
+            //booleans
+            boolean verdadero = true;
+            boolean falso = false;
+
+            //cadenas de texto
+            String nombre = "Nicolás";
+            String apellido = "Cocera";
+
+            //tipos envoltorio
+            Integer numero = null;
+            Long numero2 = 2L;
+
+        }
+  
 }
